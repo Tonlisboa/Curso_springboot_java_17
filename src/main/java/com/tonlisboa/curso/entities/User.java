7e6e8f63ebfd835f78e9c0b7be6e02bd4f86da34
Tonlisboa <tonlisboa@hotmail.com>
2022-09-14 21:42:35 -0300

    H2 database, test profile, JPA

--- src/main/java/com/tonlisboa/curso/entities/User.java
+++ src/main/java/com/tonlisboa/curso/entities/User.java
@@ -1,10 +1,17 @@
 package com.tonlisboa.curso.entities;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
+@Entity
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;

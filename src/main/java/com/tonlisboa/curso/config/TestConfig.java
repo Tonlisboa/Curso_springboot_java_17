9c9a79b69ae15e778cc938b7b89a9e9f3c17e4bb
Tonlisboa <tonlisboa@hotmail.com>
2022-09-15 18:42:43 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/tonlisboa/curso/config/TestConfig.java
+++ src/main/java/com/tonlisboa/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.tonlisboa.curso.entities.Category;
 import com.tonlisboa.curso.entities.Order;
+import com.tonlisboa.curso.entities.OrderItem;
 import com.tonlisboa.curso.entities.Product;
 import com.tonlisboa.curso.entities.User;
 import com.tonlisboa.curso.entities.enums.OrderStatus;
 import com.tonlisboa.curso.repositories.CategoryRepository;
+import com.tonlisboa.curso.repositories.OrderItemRepository;
 import com.tonlisboa.curso.repositories.OrderRepository;
 import com.tonlisboa.curso.repositories.ProductRepository;
 import com.tonlisboa.curso.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -69,6 +74,13 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 	
 	}
 }
---
+++ src/main/java/com/tonlisboa/curso/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.tonlisboa.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tonlisboa.curso.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
